9bb629a77d3b112e082acdf4e14355b2c8b9ea87
[Eric] <[erocksun@bu.edu]>
2017-07-14 12:40:20 -0400

    buttons maybe

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ function plusDivs(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
+    var y = document.getElementsByClassName("captions");
     if (n > x.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none"; 
+        y[i].style.display = "none"; 
     }
     x[slideIndex-1].style.display = "block"; 
+    y[slideIndex-1].style.display = "block"; 
 }
